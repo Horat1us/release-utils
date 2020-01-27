5927b34bd65021629a46710bf6f9d412bb82f7f6
Alexander Letnikow <reclamme@gmail.com>
2020-01-27 23:35:31 +0200

    Release 3.0.0

--- utils/version.mjs
+++ utils/version.mjs
@@ -1,6 +1,11 @@
 export const regExp = /^(\d+)\.(\d+)\.(\d+)$/;
 
 export function update(index, current, separator = ".") {
-    const version = regExp.exec(current).slice(1, 4);
-    return [...version.slice(0, current), 1 + version.shift(), ...version.map(() => 0)];
+    const version = regExp.exec(current).slice(1, 4).map((i) => parseInt(i, 10));
+    return [
+        ...version.splice(0, current),
+        1 + version.shift(),
+        ...version.map(() => 0)
+    ]
+        .join(separator);
 }
