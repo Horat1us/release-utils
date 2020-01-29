bbe674b2a0cff12c447c36163fd4b4f1e088fafe
Alexander Letnikow <reclamme@gmail.com>
2020-01-29 03:01:44 +0200

    [3.2.3] Final docker-build-nginx release

--- docker-build-nginx.sh
+++ docker-build-nginx.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
+IMAGE_REPOSITORY=${IMAGE_REPOSITORY-$( (jq '.name' package.json 2>/dev/null || basename $(pwd)) | sed 's/[^a-z\/]//g')}
+
+
 if [[ -n $CODEBUILD_BUILD_ID ]]; then
     set -e;
 
     DOCKER_LOGIN=$(aws ecr get-login --no-include-email --region ${AWS_DEFAULT_REGION-"eu-central-1"} 2>/dev/null);
     $DOCKER_LOGIN 2>/dev/null;
     DOCKER_REGISTRY=$(echo $DOCKER_LOGIN | sed -e 's/.*https:\/\///');
+    IMAGE_TAG=${IMAGE_TAG-${CODEBUILD_BUILD_ID-latest}}
+else
+    DOCKER_REGISTRY=docker.io
+    IMAGE_TAG=${IMAGE_TAG-$(jq -r '.version' ./package.json 2>/dev/null)}
 fi;
 
 set -ex;
-DOCKER_IMAGE="${DOCKER_REGISTRY}/${IMAGE_REPOSITORY}:${IMAGE_TAG-${CODEBUILD_BUILD_ID}}"
-echo -e "FROM flashspys/nginx-static@085c648c774d \n COPY . ." | docker build -t $DOCKER_IMAGE --rm --compress $1
+
+DOCKER_IMAGE="${DOCKER_REGISTRY}/${IMAGE_REPOSITORY}:${IMAGE_TAG}"
+docker build -t $DOCKER_IMAGE --rm --compress -f- ${2-$(pwd)} <<EOF
+FROM flashspys/nginx-static:latest
+COPY . .
+EOF
 docker push $DOCKER_IMAGE
 printf '[{"name":"nginx","imageUri":"%s"}]' $DOCKER_IMAGE > imagedefinitions.json
